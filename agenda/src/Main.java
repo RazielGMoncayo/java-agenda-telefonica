fd88d5514363cd38372c15d3789b648f13244608
José Ian Perez Gomez <59273923+Wegenner@users.noreply.github.com>
2025-03-24 17:25:08 -0600

    Ediciones al main

--- agenda/src/Main.java
+++ agenda/src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        int menu;
+
+        System.out.println("\n");
+        System.out.println("\n");
+        System.out.println("Bienvenido a Agendrix!! Tu agenda personal");
+        System.out.println("\n");
+        System.out.println("¿Prefieres configuración personalizada o inicio rapido?");
+
+        System.out.println("1) Personalizar");
+        System.out.println("2) Iniciar (10 contactos por defecto)");
+
+        menu = new Scanner(System.in).nextInt();
 
     }
 }
