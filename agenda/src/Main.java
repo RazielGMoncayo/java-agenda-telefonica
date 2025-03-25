42432a64c719c7a3632572c6db8c58a0b7a63a4a
RazielG <razielgmoncayo@gmail.com>
2025-03-25 11:06:56 -0600

    agenda.java cambios

--- agenda/src/Main.java
+++ agenda/src/Main.java
@@ -1,5 +1,20 @@
+import com.generation.ejercicio.informacion.Agenda;
+import com.generation.ejercicio.informacion.Contacto;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre");
+        String name = sc.nextLine();
+        System.out.println("Ingrese el apellido");
+        String lastname = sc.nextLine();
+        System.out.println("Ingrese el numero");
+        int numero = sc.nextInt();
+        Contacto nuevoContacto = new Contacto(lastname,name,numero);
+        Agenda.adContacto(nuevoContacto);
 
 
     }
--- agenda/src/com/generation/ejercicio/informacion/Agenda.java
+++ agenda/src/com/generation/ejercicio/informacion/Agenda.java
@@ -1,27 +1,73 @@
 package com.generation.ejercicio.informacion;
 
-public class Agenda extends Contacto{
+import java.util.ArrayList;
 
-    public Agenda(String apellido, String name, int telefono) {
-        super(apellido, name, telefono);
+public class Agenda{
+
+    private ArrayList<Contacto> contactos = new ArrayList<Contacto>();
+    private int numContactos;
+
+    public Agenda(int numContactos) {
+        this.numContactos = numContactos;
     }
+
+    public ArrayList<Contacto> getContactos() {
+        return contactos;
+    }
+
+    public void setContactos(ArrayList<Contacto> contactos) {
+        this.contactos = contactos;
+    }
+
+    public int getNumContactos() {
+        return numContactos;
+    }
+
+    public void setNumContactos(int numContactos) {
+        this.numContactos = numContactos;
+    }
+
+    public Agenda(int numContactos){
+        this.numContactos = numContactos;
+    };
 
     // Herramientas
     // https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/util/ArrayList.html
 
-    añadirContacto(){}
+//    añadirContacto(){}
+    public static void adContacto(Contacto c){
+        if(contactos.size() >= numContactos){
+            System.out.println("Has alcanzado el limite de contactos");
+            return;
+        }else{
+            contactos.add(c);
+            System.out.println(c);
+        }
 
-    existeContacto(){}
+    }
 
-    listarContactos(){}
+//
+//    existeContacto(){}
+//    public void existeContacto(){
+//
+//    }
+//
+//    listarContactos(){}
+    public void listarContactos(){
+        for(Contacto c : contactos){
+            System.out.println(c.getApellido() + " " + c.getName() + " " + c.getTelefono());
+        }
+    }
 
-    buscaContacto(String nombre){}
-
-    eliminarContacto(Contacto c){}
-
-    modificarTelefono(String nombre, String apellido, String nuevoTelefono){}
-
-    agendaLlena(){}
-
-    espacioLibres(){}
+    }
+//
+//    buscaContacto(String nombre){}
+//
+//    eliminarContacto(Contacto c){}
+//
+//    modificarTelefono(String nombre, String apellido, String nuevoTelefono){}
+//
+//    agendaLlena(){}
+//
+//    espacioLibres(){}
 }
