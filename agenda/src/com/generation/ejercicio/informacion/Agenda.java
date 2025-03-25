00684b6adf2dfbe400f1a16869c321c5732dbb7f
Luis Miguel Vera <luis.miguel.vera.dev10@gmail.com>
2025-03-25 16:12:42 -0600

    eliminar_contacto

--- agenda/src/com/generation/ejercicio/informacion/Agenda.java
+++ agenda/src/com/generation/ejercicio/informacion/Agenda.java
@@ -1,5 +1,8 @@
 package com.generation.ejercicio.informacion;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class Agenda extends Contacto{
 
     public Agenda(String apellido, String name, int telefono) {
@@ -17,7 +20,18 @@ public class Agenda extends Contacto{
 
     buscaContacto(String nombre){}
 
-    eliminarContacto(Contacto c){}
+    eliminarContacto(Contacto c){
+        public boolean eliminarContacto(Contacto c) {
+            if (contactos.removeIf(contacto ->
+                    contacto.getName().equalsIgnoreCase(c.getName()) &&
+                            contacto.getApellido().equalsIgnoreCase(c.getApellido()))) {
+                System.out.println("Contacto eliminado.");
+                return true;
+            }
+            System.out.println("El contacto no existe en la agenda.");
+            return false;
+        }
+    }
 
     modificarTelefono(String nombre, String apellido, String nuevoTelefono){}
 
