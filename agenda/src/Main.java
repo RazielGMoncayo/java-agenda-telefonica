ef569dc92f5d4687c4e6199e87650ae75db7da40
christian lira <lic.lirag@gmail.com>
2025-03-25 11:08:59 -0600

    buscar contactos

--- agenda/src/Main.java
+++ agenda/src/Main.java
@@ -1,9 +1,14 @@
+
+
+import com.generation.ejercicio.informacion.Agenda;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         int menu;
+        Agenda agenda = new Agenda("", "", 0); // Creamos una agenda vacía
 
         System.out.println("\n");
         System.out.println("\n");
@@ -14,7 +19,14 @@ public class Main {
         System.out.println("1) Personalizar");
         System.out.println("2) Iniciar (10 contactos por defecto)");
 
-        menu = new Scanner(System.in).nextInt();
+        menu = scanner.nextInt(); // Corregido: usa el scanner existente
 
+        // buscaContacto después de la seleccion del menu
+        System.out.println("\nPrueba de búsqueda de contacto:");
+        System.out.println("Ingrese un nombre a buscar:");
+        scanner.nextLine(); // Limpiar buffer
+        String nombreBuscar = scanner.nextLine();
+
+        agenda.buscaContacto(nombreBuscar); // Llamada al método de búsqueda
     }
 }
--- agenda/src/com/generation/ejercicio/informacion/Agenda.java
+++ agenda/src/com/generation/ejercicio/informacion/Agenda.java
@@ -1,27 +1,54 @@
 package com.generation.ejercicio.informacion;
 
-public class Agenda extends Contacto{
+import java.util.ArrayList;
+
+public class Agenda extends Contacto {
+    private ArrayList<Contacto> contactos;
 
     public Agenda(String apellido, String name, int telefono) {
         super(apellido, name, telefono);
+        this.contactos = new ArrayList<>();
     }
 
     // Herramientas
     // https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/util/ArrayList.html
 
-    añadirContacto(){}
+    void añadirContacto() {
+    }
 
-    existeContacto(){}
+    void existeContacto() {
+    }
 
-    listarContactos(){}
+    void listarContactos() {
+    }
 
-    buscaContacto(String nombre){}
+    public void buscaContacto(String nombre) {
+        boolean encontrado = false;
+        for (Contacto contacto : contactos) {
+            if (contacto.getName().equalsIgnoreCase(nombre)) {
+                System.out.println("Contacto encontrado:");
+                System.out.println("Nombre: " + contacto.getName());
+                System.out.println("Apellido: " + contacto.getApellido());
+                System.out.println("Teléfono: " + contacto.getTelefono());
+                encontrado = true;
+                break;
+            }
+        }
 
-    eliminarContacto(Contacto c){}
+        if (!encontrado) {
+            System.out.println("No se encontró ningún contacto con el nombre: " + nombre);
+        }
+    }
 
-    modificarTelefono(String nombre, String apellido, String nuevoTelefono){}
+    void eliminarContacto(Contacto c) {
+    }
 
-    agendaLlena(){}
+    void modificarTelefono(String nombre, String apellido, String nuevoTelefono) {
+    }
 
-    espacioLibres(){}
+    void agendaLlena() {
+    }
+
+    void espacioLibres() {
+    }
 }
